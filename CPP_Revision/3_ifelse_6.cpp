972802a792e22c916c08cbe7a8dafa47cb8c8511
bhuone <68323935+bhuuvan@users.noreply.github.com>
2024-04-17 13:44:54 -0500

    Add files via upload

---
+++ CPP_Revision/3_ifelse_6.cpp
@@ -0,0 +1,39 @@
+#include"bits/stdc++.h"
+using namespace std;
+/*
+Tkae the age from the user and then decide accordingly
+1. if age<18
+not eligble for job
+2. if age>=19
+eligigle for job
+3. if age>=55 and age<=57
+eligible for the job, but reitrement soon
+4. if age>57
+retirement time
+
+*/
+//Method 3
+int main(){
+    int age;
+    cin>> age;
+    if(age<18){
+        cout<<"Not eligible for job"<<endl;
+    }
+    else if (age<=54) {
+        cout<<"Eligible for job"<<endl;
+    
+    
+    }
+    else if(age<=57){
+            cout<<"Retirement soom"<<endl;
+        }
+    else {
+            cout<<"Retirement time"<<endl;
+        }
+    
+    return 0;
+    }
+    
+    
+   
+    
