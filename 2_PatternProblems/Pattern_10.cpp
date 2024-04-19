ad0c62a85b60da6eb6e96d4f017f852995320275
bhuone <68323935+bhuuvan@users.noreply.github.com>
2024-04-18 23:09:28 -0500

    Add files via upload

---
+++ 2_PatternProblems/Pattern_10.cpp
@@ -0,0 +1,26 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+
+void pattern10(int n){
+    
+    for(int i =1; i<=2*n-1; i++){
+        int stars =i;
+        if(i>n) stars = 2*n-i;
+        for(int j =1; j<=stars; j++){
+            cout<<" *";
+        }
+
+
+        cout<<endl;
+    }
+
+
+}
+
+int main(){
+
+
+    pattern10(5);
+    return 0;
+}
