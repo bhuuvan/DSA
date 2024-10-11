0d744cd17bb627f80679d4c2552b9cf200107283
bhuone <68323935+bhuuvan@users.noreply.github.com>
2024-10-10 20:45:04 -0500

    Add files via upload

---
+++ 3_STL/1_oneshot_2darray.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main(){
+
+    int arr[3][5];
+
+    arr[2][0]= 45;
+
+    cout<<arr[2][0]<<endl;
+    cout<<arr[2][2];
+    
+
+
+    return 0;
+}
